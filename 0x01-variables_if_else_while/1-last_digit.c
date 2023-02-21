9c06d1ffc2b19bcad90b8cd278a547faf5a2313c
Nebeyu Hail <nebeyuh3@gmail.com>
2023-02-20 21:32:11 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * more headers goes there
- *Discription: 'get the last degiti of a number'
- *Return: always 0
- *
- */
+/**
+  *main - Entry point
+  *Discription: 'get the last degiti of a number'
+  *Return: always 0
+  */
 int main(void)
 {
 	int n;
