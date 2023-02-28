d66e38802ea0f4ff7a81b8a924c1fb717c04cf55
Nebeyu Hail <nebeyuh3@gmail.com>
2023-02-28 12:47:56 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,27 +8,18 @@
 
 void puts_half(char *str)
 {
-	int len, n, i;
+	int i = 0;
+	int n = 0;
 
-	len = 0;
-
-	while (str[len] != '\n')
+	while (str[i] != '\0')
+		i += 1;
+	n = i / 2;
+	if (i % 2 == 1)
+		n += 1;
+	while (str[n] != '\0')
 	{
-		len++;
+		putchar(*(str + n));
+		n++;
 	}
-	if (len % 2 == 0)
-	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			putchar(str[i]);
-		}
-	}
-	else if (len % 2)
-	{
-		for (n = (len - 1) / 2; n < len - 1; i++)
-		{
-			putchar(str[i]);
-		}
-	}
-	putchar('\n');
+	putchar('\0');
 }
