dfc89e47075b819a53943395a4c3a837f8c18e14
Nebeyu Hail <nebeyuh3@gmail.com>
2023-03-02 16:34:16 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,6 +3,7 @@
 /**
  * print_number -prints an integer
  * @n: integer to be printed
+ * Return: 0
  */
 void print_number(int n)
 {
@@ -13,8 +14,7 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
-
-	if ((num / 10) > 0)
+	if (num / 10 != 0)
 		print_number(num / 10);
 
 	_putchar((num % 10) + '0');
